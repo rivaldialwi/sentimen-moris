94b42b0482fa93b9d3e99c60d11bb5ef4c91f7be
unknown <rivaldialwi.com>
2024-07-09 09:12:14 +0700

    bismillah

--- laporan.py
+++ laporan.py
@@ -18,7 +18,7 @@ def download_nltk_resources():
 download_nltk_resources()
 
 # Daftar kata yang akan diganti atau dihapus
-replace_words = {
+replace_words_negatif = {
     "yang": "",
     "nya": "",
     "kok": "",
@@ -30,27 +30,177 @@ replace_words = {
     "doang": "",
     "midnight": "",
     "minggu": "",
+    "kak": "",
+    "beli": "",
+    "puas": "",
+    "enak": "",
+    "pas": "",
+    "suka": "",
+    "pure": "",
+    "jakarta": "",
+    "kalo": "",
+    "jam": "",
+    "wangi": "",
+    "puluh": "",
+    "tiup": "",
+    "langgan": "",
+    "kena": "",
+    "layan": "",
+    "kemaren": "",
+    "sesuai": "",
+    "putih": "",
+    "parfum": "",
+    "tahan": "",
+    "jalan": "",
+    "angin": "",
+    "cepat": "",
+    "cepet": "",
+    "kaya": "",
+    "aroma": "",
+    "tinggal": "",
+    "buka": "",
+    "sudah": "",
+    "sampe": "",
+    "barang": "",
+    "menit": "",
+    "express": "",
+    "percaya": "",
+    "make": "",
+    "ekspektasi": "",
+    "banget": "",
+    "harga": "",
+    "pake": "",
+    "woody": "",
+    "kotak": "",
+    "moris": "",
+    "jasa": "",
+    "alfamart": "",
+    "belanja": "",
+    "kuat": "",
+    "botol": "",
+    "ongkos": "",
+    "estimasi": "",
+    "kaos": "",
+    "leher": "",
+    "pakai": "",
+    "tutup": "",
+    "delapan": "",
+    "modal": "",
+    "layak": "",
+    "murah": "",
+    "bijak": "",
+    "iklan": "",
+    "box": "",
+    "kardus": "",
+    "kembali": "",
+    "harum": "",
+    "metropolis": "",
+    "kepala": "",
+    "kualitas": "",
+    "pesan": "",
+    "semprot": "",
+    "kirim": "",
+    "massa": "",
+    "hubung": "",
+    "rumah": "",
+    "nyata": "",
+    "mudah": "",
+    "sangat": "",
+    "worth": "",
+    "sengat": "",
+    "baju": "",
+    "ka": "",
     "ga": "tidak",
     "gak": "tidak",
     "tidakk": "tidak",
     "udah": "sudah",
-    "ka": "kak",
-    "kakk": "kak",
     "cewe": "cewek",
     "cew": "cewek",
     "cowo": "cowok",
     "cow": "cowok",
 }
 
+replace_words_netral = {
+    "ruang": "",
+    "campur": "",
+    "best": "",
+    "ibu": "",
+    "ga": "",
+    "gak": "",
+    "nya": "",
+    "kalo": "",
+    "ka": "",
+    "kak": "",
+    "bau": "",
+    "borong": "",
+    "bener": "",
+    "yah": "",
+}
+
+
+replace_words_positif = {
+    "harga": "",
+    "ka": "",
+    "kak": "",
+    "biar": "",
+    "segitu": "",
+    "kelas": "",
+    "estimasi": "",
+    "pakai": "",
+    "baju": "",
+    "se": "",
+    "nya": "",
+    "emang": "",
+    "biru": "",
+    "pusing": "",
+    "bismillah": "",
+    "udah": "",
+    "kepala": "",
+    "tajam": "",
+    "kirim": "",
+    "sekolah": "",
+    "belah": "",
+    "anak": "",
+    "bikin": "",
+    "botol": "",
+    "jodoh": "",
+    "motif": "",
+    "parfum": "",
+    "beli": "",
+    "varian": "",
+    "breeze": "",
+    "gak": "",
+    "ocean": "",
+    "keluarga": "",
+    "metropolis": "",
+    "woody": "",
+    "morris": "",
+    "pas": "",
+    "orang": "",
+    "langsung": "",
+    "deh": "",
+    "pake": "",
+    "sih": "",
+    "jalan": "",
+    "pokok": "",
+    "moris": "",
+    "sesal": "",
+    "sumpah": "",
+    "artis": "",
+    "asa": "",
+    "asik": "",
+}
+
+
 # Fungsi untuk mengganti atau menghapus kata-kata tertentu dalam teks
-def replace_and_remove_words(text):
+def replace_and_remove_words(text, replace_words):
     words = text.split()
     replaced_words = [replace_words.get(word, word) for word in words]
     return " ".join(replaced_words)
 
 # Fungsi untuk membuat word cloud
 def create_word_cloud(text, title):
-    wordcloud = WordCloud(width=200, height=200, background_color='white').generate(text)
+    wordcloud = WordCloud(width=300, height=200, background_color='white').generate(text)
     fig, ax = plt.subplots()
     ax.imshow(wordcloud, interpolation='bilinear')
     ax.axis('off')
@@ -58,7 +208,7 @@ def create_word_cloud(text, title):
     st.pyplot(fig)
 
 # Fungsi untuk membersihkan teks
-def clean_text(text):
+def clean_text(text, replace_words):
     stop_words = set(stopwords.words('indonesian'))
     factory = StemmerFactory()
     stemmer = factory.create_stemmer()
@@ -66,7 +216,7 @@ def clean_text(text):
     words = word_tokenize(text)  # Tokenizing
     cleaned_words = [word for word in words if word not in stop_words]  # Stopword removal
     stemmed_words = [stemmer.stem(word) for word in cleaned_words]  # Stemming
-    replaced_text = replace_and_remove_words(" ".join(stemmed_words))  # Replace and remove words
+    replaced_text = replace_and_remove_words(" ".join(stemmed_words), replace_words)  # Replace and remove words
     return replaced_text
 
 # Fungsi untuk menjalankan Website
@@ -108,7 +258,18 @@ def run():
             sentiments = df_excel['Human'].unique()
             for sentiment in sentiments:
                 sentiment_text = " ".join(df_excel[df_excel['Human'] == sentiment]['Text'])
-                sentiment_text_cleaned = clean_text(sentiment_text)
+                
+                # Pilih daftar kata yang akan diganti atau dihapus berdasarkan sentimen
+                if sentiment == 'Negatif':
+                    replace_words = replace_words_negatif
+                elif sentiment == 'Netral':
+                    replace_words = replace_words_netral
+                elif sentiment == 'Positif':
+                    replace_words = replace_words_positif
+                else:
+                    replace_words = {}
+
+                sentiment_text_cleaned = clean_text(sentiment_text, replace_words)
                 create_word_cloud(sentiment_text_cleaned, f'Word Cloud untuk Sentimen {sentiment}')
         else:
             st.error("File harus memiliki kolom 'Human'.")
